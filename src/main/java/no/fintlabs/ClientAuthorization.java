19d1f263f8cf22bcf10f2e0b2878f58733825da0
Nils H Opsahl <nils.hakon.opsahl@vigoiks.no>
2023-06-05 14:55:36 +0200

    Remove annotations. rename to groovy

--- src/main/java/no/fintlabs/ClientAuthorization.java
+++ src/main/java/no/fintlabs/ClientAuthorization.java
@@ -1,14 +1,9 @@
 package no.fintlabs;
 
 import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.extern.jackson.Jacksonized;
 
 @Getter
-@EqualsAndHashCode
-@Jacksonized
 @Builder
 public class ClientAuthorization {
     private final boolean authorized;
