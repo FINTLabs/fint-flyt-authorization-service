fcff878a6ed9faec3a0807f60e38e26807d58f3a
Egil Ballestad <egil.ballestad@vigoiks.no>
2023-12-12 13:23:05 +0100

    add support for rf1350 clientId authorization with sourceApplication ID and cleanup viken deployments

--- src/main/java/no/fintlabs/ClientAuthorizationProducerRecordBuilder.java
+++ src/main/java/no/fintlabs/ClientAuthorizationProducerRecordBuilder.java
@@ -3,6 +3,7 @@ package no.fintlabs;
 import no.fintlabs.kafka.requestreply.ReplyProducerRecord;
 import no.fintlabs.models.AcosSourceApplication;
 import no.fintlabs.models.EgrunnervervSourceApplication;
+import no.fintlabs.models.RF1350SourceApplication;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +14,12 @@ public class ClientAuthorizationProducerRecordBuilder {
 
     public ReplyProducerRecord<ClientAuthorization> apply(ConsumerRecord<String, String> consumerRecord) {
 
-        if (AcosSourceApplication.CLIENT_ID != null && Objects.equals(consumerRecord.value(), AcosSourceApplication.CLIENT_ID)){
+        if (AcosSourceApplication.CLIENT_ID != null && Objects.equals(consumerRecord.value(), AcosSourceApplication.CLIENT_ID)) {
             return buildReplyProducerRecord(AcosSourceApplication.CLIENT_ID, AcosSourceApplication.SOURCE_APPLICATION_ID);
-        } else if (EgrunnervervSourceApplication.CLIENT_ID != null && Objects.equals(consumerRecord.value(), EgrunnervervSourceApplication.CLIENT_ID)){
+        } else if (EgrunnervervSourceApplication.CLIENT_ID != null && Objects.equals(consumerRecord.value(), EgrunnervervSourceApplication.CLIENT_ID)) {
             return buildReplyProducerRecord(EgrunnervervSourceApplication.CLIENT_ID, EgrunnervervSourceApplication.SOURCE_APPLICATION_ID);
+        } else if (RF1350SourceApplication.CLIENT_ID != null && Objects.equals(consumerRecord.value(), RF1350SourceApplication.CLIENT_ID)) {
+            return buildReplyProducerRecord(RF1350SourceApplication.CLIENT_ID, RF1350SourceApplication.SOURCE_APPLICATION_ID);
         } else {
             return ReplyProducerRecord.<ClientAuthorization>builder()
                     .value(ClientAuthorization
@@ -28,7 +31,7 @@ public class ClientAuthorizationProducerRecordBuilder {
         }
     }
 
-    private ReplyProducerRecord<ClientAuthorization> buildReplyProducerRecord(String clientId, String sourceApplicationId){
+    private ReplyProducerRecord<ClientAuthorization> buildReplyProducerRecord(String clientId, String sourceApplicationId) {
         return ReplyProducerRecord.<ClientAuthorization>builder()
                 .value(ClientAuthorization
                         .builder()
