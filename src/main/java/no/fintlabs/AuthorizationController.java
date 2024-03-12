973ee7283a6c4b2b12ad587f0ac8022d8175ae75
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-03-12 13:17:41 +0100

    add admin-role and use new rc of fint-flyt-resource-server

--- src/main/java/no/fintlabs/AuthorizationController.java
+++ src/main/java/no/fintlabs/AuthorizationController.java
@@ -1,15 +1,20 @@
 package no.fintlabs;
 
+import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.models.AuthorizedUser;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import static no.fintlabs.resourceserver.UrlPaths.INTERNAL_API;
 
 @RestController
 @RequestMapping(INTERNAL_API + "/authorization")
+@Slf4j
 public class AuthorizationController {
 
     public AuthorizationController() {
@@ -21,8 +26,17 @@ public class AuthorizationController {
     }
 
     @GetMapping("user")
-    public ResponseEntity<AuthorizedUser> checkUser() {
-        return ResponseEntity.ok(AuthorizedUser.builder().admin(true).build());
+    public Mono<ResponseEntity<AuthorizedUser>> checkUser() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .flatMap(authentication -> {
+                    if (authentication != null && authentication.getAuthorities().stream()
+                            .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"))) {
+                        return Mono.just(ResponseEntity.ok(AuthorizedUser.builder().admin(true).build()));
+                    } else {
+                        return Mono.just(ResponseEntity.ok(AuthorizedUser.builder().admin(false).build()));
+                    }
+                });
     }
 
 }
