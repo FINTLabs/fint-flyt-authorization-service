1da53eb8590f2f97bdcb80a112271f8e7492d82b
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-06-26 08:54:43 +0200

    remove possibility to create new users on endpoint for setting user permissions

--- src/main/java/no/fintlabs/flyt/azure/services/AzureAppGraphService.java
+++ src/main/java/no/fintlabs/flyt/azure/services/AzureAppGraphService.java
@@ -3,6 +3,7 @@ package no.fintlabs.flyt.azure.services;
 import com.microsoft.graph.models.*;
 import com.microsoft.graph.options.QueryOption;
 import com.microsoft.graph.requests.GraphServiceClient;
+import com.microsoft.graph.requests.ServicePrincipalCollectionPage;
 import okhttp3.Request;
 import org.springframework.stereotype.Service;
 
@@ -25,11 +26,20 @@ public class AzureAppGraphService {
     public List<AppRoleAssignment> getAppRoleAssignments(String appId) {
         List<QueryOption> requestOptions = new ArrayList<>();
         requestOptions.add(new QueryOption("$filter", "appId eq '" + appId + "'"));
-        List<ServicePrincipal> servicePrincipals = Objects.requireNonNull(graphService
-                        .servicePrincipals()
-                        .buildRequest(requestOptions)
-                        .get())
-                .getCurrentPage();
+
+        ServicePrincipalCollectionPage servicePrincipalCollectionPage = graphService
+                .servicePrincipals()
+                .buildRequest(requestOptions)
+                .get();
+
+        Objects.requireNonNull(servicePrincipalCollectionPage);
+
+//        if (servicePrincipalCollectionPage.getCount()) {
+//
+//        }
+
+        List<ServicePrincipal> servicePrincipals = servicePrincipalCollectionPage.getCurrentPage();
+
         if (!servicePrincipals.isEmpty()) {
             ServicePrincipal servicePrincipal = servicePrincipals.get(0);
             if (servicePrincipal != null && servicePrincipal.id != null) {
