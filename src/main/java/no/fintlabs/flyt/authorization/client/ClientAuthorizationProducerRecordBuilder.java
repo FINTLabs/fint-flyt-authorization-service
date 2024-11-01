f311f854e8fb40d618ddad6be8a4a44ad2369e21
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-11-01 14:38:58 +0100

    add altinn as source application with id 5

--- src/main/java/no/fintlabs/flyt/authorization/client/ClientAuthorizationProducerRecordBuilder.java
+++ src/main/java/no/fintlabs/flyt/authorization/client/ClientAuthorizationProducerRecordBuilder.java
@@ -1,9 +1,6 @@
 package no.fintlabs.flyt.authorization.client;
 
-import no.fintlabs.flyt.authorization.client.sourceapplications.AcosSourceApplication;
-import no.fintlabs.flyt.authorization.client.sourceapplications.DigisakSourceApplication;
-import no.fintlabs.flyt.authorization.client.sourceapplications.EgrunnervervSourceApplication;
-import no.fintlabs.flyt.authorization.client.sourceapplications.VigoSourceApplication;
+import no.fintlabs.flyt.authorization.client.sourceapplications.*;
 import no.fintlabs.kafka.requestreply.ReplyProducerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.stereotype.Component;
@@ -23,6 +20,8 @@ public class ClientAuthorizationProducerRecordBuilder {
             return buildReplyProducerRecord(DigisakSourceApplication.CLIENT_ID, DigisakSourceApplication.SOURCE_APPLICATION_ID);
         } else if (VigoSourceApplication.CLIENT_ID != null && Objects.equals(consumerRecord.value(), VigoSourceApplication.CLIENT_ID)) {
             return buildReplyProducerRecord(VigoSourceApplication.CLIENT_ID, VigoSourceApplication.SOURCE_APPLICATION_ID);
+        } else if (AltinnSourceApplication.CLIENT_ID != null && Objects.equals(consumerRecord.value(), AltinnSourceApplication.CLIENT_ID)) {
+            return buildReplyProducerRecord(AltinnSourceApplication.CLIENT_ID, AltinnSourceApplication.SOURCE_APPLICATION_ID);
         } else {
             return ReplyProducerRecord.<ClientAuthorization>builder()
                     .value(ClientAuthorization
