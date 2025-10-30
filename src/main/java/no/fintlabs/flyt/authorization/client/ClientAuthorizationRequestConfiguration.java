7402fe58ddf287bb17505fb85fabd0dd07a74e07
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-10-30 15:59:57 +0100

    wip
    Bump to newest fint-kafka and resource-server, which is using Spring Boot 3.5

--- src/main/java/no/fintlabs/flyt/authorization/client/ClientAuthorizationRequestConfiguration.java
+++ src/main/java/no/fintlabs/flyt/authorization/client/ClientAuthorizationRequestConfiguration.java
@@ -1,17 +1,24 @@
 package no.fintlabs.flyt.authorization.client;
 
-import no.fintlabs.kafka.common.topic.TopicCleanupPolicyParameters;
-import no.fintlabs.kafka.requestreply.RequestConsumerFactoryService;
-import no.fintlabs.kafka.requestreply.topic.RequestTopicNameParameters;
+import no.fintlabs.kafka.consuming.ErrorHandlerConfiguration;
+import no.fintlabs.kafka.consuming.ErrorHandlerFactory;
+import no.fintlabs.kafka.requestreply.RequestListenerConfiguration;
+import no.fintlabs.kafka.requestreply.RequestListenerContainerFactory;
 import no.fintlabs.kafka.requestreply.topic.RequestTopicService;
+import no.fintlabs.kafka.requestreply.topic.configuration.RequestTopicConfiguration;
+import no.fintlabs.kafka.requestreply.topic.name.RequestTopicNameParameters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.time.Duration;
 
 @Configuration
 public class ClientAuthorizationRequestConfiguration {
 
     private final ClientAuthorizationProducerRecordBuilder recordBuilder;
+
+    private static final Duration RETENTION_TIME = Duration.ofMinutes(10);
 
     public ClientAuthorizationRequestConfiguration(ClientAuthorizationProducerRecordBuilder recordBuilder) {
         this.recordBuilder = recordBuilder;
@@ -20,19 +27,33 @@ public class ClientAuthorizationRequestConfiguration {
     @Bean
     public ConcurrentMessageListenerContainer<String, String> clientAuthorizationRequestConsumer(
             RequestTopicService requestTopicService,
-            RequestConsumerFactoryService requestConsumerFactoryService
-    ) {
+            RequestListenerContainerFactory requestListenerContainerFactory,
+            ErrorHandlerFactory errorHandlerFactory) {
         RequestTopicNameParameters topicNameParameters = RequestTopicNameParameters.builder()
-                .resource("authorization")
+                .resourceName("authorization")
                 .parameterName("client-id")
                 .build();
 
-        requestTopicService.ensureTopic(topicNameParameters, 0, TopicCleanupPolicyParameters.builder().build());
+        requestTopicService.createOrModifyTopic(topicNameParameters, RequestTopicConfiguration.builder()
+                .retentionTime(RETENTION_TIME)
+                .build()
+        );
 
-        return requestConsumerFactoryService.createRecordConsumerFactory(
+        return requestListenerContainerFactory.createRecordConsumerFactory(
                 String.class,
                 ClientAuthorization.class,
-                (recordBuilder::apply)
+                (recordBuilder::apply),
+                RequestListenerConfiguration
+                        .stepBuilder(String.class)
+                        .maxPollRecordsKafkaDefault()
+                        .maxPollIntervalKafkaDefault()
+                        .build(),
+                errorHandlerFactory.createErrorHandler(ErrorHandlerConfiguration
+                        .stepBuilder()
+                        .noRetries()
+                        .skipFailedRecords()
+                        .build())
+
         ).createContainer(topicNameParameters);
     }
 
--- src/main/java/no/fintlabs/flyt/authorization/user/kafka/UserPermissionEntityProducerService.java
+++ src/main/java/no/fintlabs/flyt/authorization/user/kafka/UserPermissionEntityProducerService.java
@@ -1,37 +1,47 @@
 package no.fintlabs.flyt.authorization.user.kafka;
 
-import no.fintlabs.kafka.entity.EntityProducer;
-import no.fintlabs.kafka.entity.EntityProducerFactory;
-import no.fintlabs.kafka.entity.EntityProducerRecord;
-import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
-import no.fintlabs.kafka.entity.topic.EntityTopicService;
+import no.fintlabs.kafka.model.ParameterizedProducerRecord;
+import no.fintlabs.kafka.producing.ParameterizedTemplate;
+import no.fintlabs.kafka.producing.ParameterizedTemplateFactory;
+import no.fintlabs.kafka.topic.EntityTopicService;
+import no.fintlabs.kafka.topic.configuration.EntityCleanupFrequency;
+import no.fintlabs.kafka.topic.configuration.EntityTopicConfiguration;
+import no.fintlabs.kafka.topic.name.EntityTopicNameParameters;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 @Service
 public class UserPermissionEntityProducerService {
 
-    private final EntityProducer<UserPermission> entityProducer;
+    private final ParameterizedTemplate<UserPermission> parameterizedTemplate;
     private final EntityTopicNameParameters entityTopicNameParameters;
 
+    private static final Duration RETENTION_TIME = Duration.ofDays(1);
+
     public UserPermissionEntityProducerService(
-            EntityProducerFactory entityProducerFactory,
+            ParameterizedTemplateFactory parameterizedTemplateFactory,
             EntityTopicService entityTopicService
     ) {
-        this.entityProducer = entityProducerFactory.createProducer(UserPermission.class);
+        this.parameterizedTemplate = parameterizedTemplateFactory.createTemplate(UserPermission.class);
+
         entityTopicNameParameters = EntityTopicNameParameters
                 .builder()
-                .resource("userpermission")
+                .resourceName("userpermission")
                 .build();
-        int retentionTimeInHours = 24;
-        long retentionTimeInMilliseconds = TimeUnit.HOURS.toMillis(retentionTimeInHours);
-        entityTopicService.ensureTopic(entityTopicNameParameters, retentionTimeInMilliseconds);
+
+        entityTopicService.createOrModifyTopic(entityTopicNameParameters, EntityTopicConfiguration.builder()
+                .partitions(1)
+                .lastValueRetentionTime(RETENTION_TIME)
+                .nullValueRetentionTime(RETENTION_TIME)
+                .cleanupFrequency(EntityCleanupFrequency.FREQUENT)
+                .build()
+        );
     }
 
     public void send(UserPermission userPermission) {
-        entityProducer.send(
-                EntityProducerRecord.<UserPermission>builder()
+        parameterizedTemplate.send(
+                ParameterizedProducerRecord.<UserPermission>builder()
                         .topicNameParameters(entityTopicNameParameters)
                         .key(String.valueOf(userPermission.getObjectIdentifier()))
                         .value(userPermission)
