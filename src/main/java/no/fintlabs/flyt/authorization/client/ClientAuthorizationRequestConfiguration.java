eb52cfc785ea0566c3c190dee2055746d6b5d643
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-10-31 10:57:38 +0100

    wip
    Must specify topicNamePrefixParameters

--- src/main/java/no/fintlabs/flyt/authorization/client/ClientAuthorizationRequestConfiguration.java
+++ src/main/java/no/fintlabs/flyt/authorization/client/ClientAuthorizationRequestConfiguration.java
@@ -7,6 +7,7 @@ import no.fintlabs.kafka.requestreply.RequestListenerContainerFactory;
 import no.fintlabs.kafka.requestreply.topic.RequestTopicService;
 import no.fintlabs.kafka.requestreply.topic.configuration.RequestTopicConfiguration;
 import no.fintlabs.kafka.requestreply.topic.name.RequestTopicNameParameters;
+import no.fintlabs.kafka.topic.name.TopicNamePrefixParameters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
@@ -30,6 +31,11 @@ public class ClientAuthorizationRequestConfiguration {
             RequestListenerContainerFactory requestListenerContainerFactory,
             ErrorHandlerFactory errorHandlerFactory) {
         RequestTopicNameParameters topicNameParameters = RequestTopicNameParameters.builder()
+                .topicNamePrefixParameters(TopicNamePrefixParameters
+                        .builder()
+                        .orgIdApplicationDefault()
+                        .domainContextApplicationDefault()
+                        .build())
                 .resourceName("authorization")
                 .parameterName("client-id")
                 .build();
