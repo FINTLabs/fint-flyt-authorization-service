033bb00256d4d577d28ab6a75efddd64b07705e7
Egil Ballestad <egil.ballestad@vigoiks.no>
2025-12-16 13:47:30 +0100

    refactor and fix bug on publishing user permissions

--- src/main/java/no/novari/flyt/authorization/user/UserPublishingComponent.java
+++ src/main/java/no/novari/flyt/authorization/user/UserPublishingComponent.java
@@ -1,20 +1,18 @@
 package no.novari.flyt.authorization.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(value = "novari.flyt.authorization.access-control.enabled", havingValue = "true")
 @RequiredArgsConstructor
 public class UserPublishingComponent {
 
     private final UserService userService;
 
     @Scheduled(
-            initialDelayString = "${novari.flyt.authorization.access-control.sync-schedule.initial-delay-ms}",
-            fixedDelayString = "${novari.flyt.authorization.access-control.sync-schedule.fixed-delay-ms}"
+            initialDelayString = "${novari.flyt.authorization.access-control.sync-schedule.initial-delay}",
+            fixedDelayString = "${novari.flyt.authorization.access-control.sync-schedule.fixed-delay}"
     )
     public void publishUsers() {
         userService.publishUsers();
