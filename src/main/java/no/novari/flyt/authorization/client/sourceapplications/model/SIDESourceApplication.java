7fb123d6391a68cbc02bd80f5670bbff38e5eed3
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-02-02 13:40:22 +0100

    add fskyss and side

---
+++ src/main/java/no/novari/flyt/authorization/client/sourceapplications/model/SIDESourceApplication.java
@@ -0,0 +1,14 @@
+package no.novari.flyt.authorization.client.sourceapplications.model;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SIDESourceApplication extends BaseSourceApplication {
+    public SIDESourceApplication(
+            @Value("${fint.flyt.side.sso.client-id:#{null}}") String clientId,
+            @Value("${fint.flyt.side.available:true}") boolean available
+    ) {
+        super(9L, "sIDE", clientId, available);
+    }
+}
