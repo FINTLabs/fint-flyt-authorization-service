b6dd84004fc309bf254b3a878765fea2691e744e
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-07-06 14:43:49 +0200

    legg til støtte for eapply gateway

---
+++ src/main/kotlin/no/novari/flyt/authorization/client/sourceapplications/model/EApplySourceApplication.kt
@@ -0,0 +1,17 @@
+package no.novari.flyt.authorization.client.sourceapplications.model
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class EApplySourceApplication(
+    @Value("\${fint.flyt.eapply.sso.client-id:#{null}}")
+    clientId: String?,
+    @Value("\${fint.flyt.eapply.available:false}")
+    available: Boolean,
+) : BaseSourceApplication(
+        id = 10L,
+        displayName = "eApply",
+        clientId = clientId,
+        available = available,
+    )
